b2e230106770f7ce72cb00ff0e4be7446cb7df87
Ryan Loudfoot <ryan@delotha.com>
2013-11-02 12:30:25 -0400

    Forgot to update version number last update. Fixed.

--- check_temp.sh
+++ check_temp.sh
@@ -33,7 +33,7 @@
 #                                                                             #
 ###############################################################################
 
-VERSION="Version 0.8"
+VERSION="Version 0.9"
 AUTHOR="(c) 2011 Jack-Benny Persson (jack-benny@cyberinfo.se)"
 
 # Sensor program
